aa3b7e06088ef6aa58648e17545b0a804901c041
Cecilia/Mimi Chung <ceciliayu.chung@gmail.com>
2020-06-19 12:14:49 -0700

    added ls and cat functionalities

--- bash.js
+++ bash.js
@@ -1,13 +1,19 @@
 const { pwd } = require('./pwd')
-
+const { ls } = require('./ls')
+const { cat } = require('./cat')
 
 process.stdout.write('prompt > ')
 
 process.stdin.on('data', data => {
-  const cmd = data.toString().trim()
-  if (cmd === 'pwd') {
+  const cmd = data.toString().trim().split(' ');
+  if (cmd[0] === 'pwd') {
     pwd()
-  } else {
+  } else if (cmd[0] === 'ls') {
+    ls()
+  } else if (cmd[0] === 'cat') {
+    cat(cmd[1])
+  }
+  else {
   process.stdout.write('you typed: ' + cmd)
   process.stdout.write('\nprompt > ')
   }
--- pwd.js
+++ pwd.js
@@ -1,6 +1,6 @@
-module.exports.pwd = pwd
-
 function pwd() {
   process.stdout.write(process.cwd())
   process.stdout.write('\nprompt > ')
 }
+
+module.exports.pwd = pwd
